942c8791a5ec86f7109a054cd07d467a9dbe314d
Yvaris Djouda <yvaris.djouda@coodoo.de>
2021-02-22 15:31:22 +0100

    visibility of method type2Class changed

--- src/main/java/io/coodoo/workhorse/core/boundary/WorkerWith.java
+++ src/main/java/io/coodoo/workhorse/core/boundary/WorkerWith.java
@@ -15,8 +15,10 @@ import io.coodoo.workhorse.core.entity.Execution;
 import io.coodoo.workhorse.util.WorkhorseUtil;
 
 /**
- * worker class to define the creation and processing of execution with parameters. <br>
- * <tt>T</tt> can be any Object or a {@link List} of {@link String} of {@link Integer} <br>
+ * worker class to define the creation and processing of execution with
+ * parameters. <br>
+ * <tt>T</tt> can be any Object or a {@link List} of {@link String} of
+ * {@link Integer} <br>
  * Your job does not need parameters? See {@link Worker}!
  * 
  * @author coodoo GmbH (coodoo.io)
@@ -53,13 +55,16 @@ public abstract class WorkerWith<T> extends BaseWorker {
         return parametersClass;
     }
 
-    public static Class<?> type2Class(Type type) { // https://stackoverflow.com/a/48066001/4034100
+    // TODO Test complete the method
+    protected Class<?> type2Class(Type type) { // https://stackoverflow.com/a/48066001/4034100
         if (type instanceof Class) {
             return (Class<?>) type;
         } else if (type instanceof GenericArrayType) {
             // having to create an array instance to get the class is kinda nasty
-            // but apparently this is a current limitation of java-reflection concerning array classes.
-            // E.g. T[] -> T -> Object.class if <T> or Number.class if <T extends Number & Comparable>
+            // but apparently this is a current limitation of java-reflection concerning
+            // array classes.
+            // E.g. T[] -> T -> Object.class if <T> or Number.class if <T extends Number &
+            // Comparable>
             return Array.newInstance(type2Class(((GenericArrayType) type).getGenericComponentType()), 0).getClass();
         } else if (type instanceof ParameterizedType) {
             return type2Class(((ParameterizedType) type).getRawType()); // Eg. List<T> would return List.class
@@ -68,7 +73,8 @@ public abstract class WorkerWith<T> extends BaseWorker {
             return bounds.length == 0 ? Object.class : type2Class(bounds[0]); // erasure is to the left-most bound.
         } else if (type instanceof WildcardType) {
             Type[] bounds = ((WildcardType) type).getUpperBounds();
-            return bounds.length == 0 ? Object.class : type2Class(bounds[0]); // erasure is to the left-most upper bound.
+            return bounds.length == 0 ? Object.class : type2Class(bounds[0]); // erasure is to the left-most upper
+                                                                              // bound.
         } else {
             throw new UnsupportedOperationException("cannot handle type class: " + type.getClass());
         }
@@ -85,7 +91,8 @@ public abstract class WorkerWith<T> extends BaseWorker {
     /**
      * <i>Convenience method to create a job execution</i><br>
      * <br>
-     * This creates a {@link Execution} object that gets added to the job engine with default options.
+     * This creates a {@link Execution} object that gets added to the job engine
+     * with default options.
      * 
      * @param parameters needed parameters to do the job
      * @return job execution ID
@@ -95,14 +102,16 @@ public abstract class WorkerWith<T> extends BaseWorker {
     }
 
     /**
-     * <i>This is an access point to get the job engine started with a new job with job parameters.</i><br>
+     * <i>This is an access point to get the job engine started with a new job with
+     * job parameters.</i><br>
      * <br>
      * 
-     * This creates a {@link Execution} object that gets added to the job engine to be executed as soon as possible.
+     * This creates a {@link Execution} object that gets added to the job engine to
+     * be executed as soon as possible.
      * 
      * @param parameters needed parameters to do the job
-     * @param priority priority queuing
-     * @param maturity specified time for the execution
+     * @param priority   priority queuing
+     * @param maturity   specified time for the execution
      * @return job execution ID
      */
     public Long createExecution(T parameters, Boolean priority, LocalDateTime maturity) {
@@ -110,15 +119,17 @@ public abstract class WorkerWith<T> extends BaseWorker {
     }
 
     /**
-     * <i>This is an access point to get the job engine started with a new job with job parameters.</i><br>
+     * <i>This is an access point to get the job engine started with a new job with
+     * job parameters.</i><br>
      * <br>
      * 
-     * This creates a {@link Execution} object that gets added to the job engine to be executed as soon as possible.
+     * This creates a {@link Execution} object that gets added to the job engine to
+     * be executed as soon as possible.
      * 
      * @param parameters needed parameters to do the job
-     * @param priority priority queuing
+     * @param priority   priority queuing
      * @param delayValue time to wait
-     * @param delayUnit what kind of time to wait
+     * @param delayUnit  what kind of time to wait
      * @return job execution ID
      */
     public Long createExecution(T parameters, Boolean priority, Long delayValue, ChronoUnit delayUnit) {
@@ -128,7 +139,8 @@ public abstract class WorkerWith<T> extends BaseWorker {
     /**
      * <i>Convenience method to create a job execution</i><br>
      * <br>
-     * This creates a {@link Execution} object that gets added to the priority queue of the job engine to be treated first class.
+     * This creates a {@link Execution} object that gets added to the priority queue
+     * of the job engine to be treated first class.
      * 
      * @param parameters needed parameters to do the job
      * @return job execution ID
@@ -140,11 +152,12 @@ public abstract class WorkerWith<T> extends BaseWorker {
     /**
      * <i>Convenience method to create a job execution</i><br>
      * <br>
-     * This creates a {@link Execution} object that gets added to the job engine after the given delay.
+     * This creates a {@link Execution} object that gets added to the job engine
+     * after the given delay.
      * 
      * @param parameters needed parameters to do the job
      * @param delayValue time to wait
-     * @param delayUnit what kind of time to wait
+     * @param delayUnit  what kind of time to wait
      * @return job execution ID
      */
     public Long createDelayedExecution(T parameters, Long delayValue, ChronoUnit delayUnit) {
@@ -154,10 +167,11 @@ public abstract class WorkerWith<T> extends BaseWorker {
     /**
      * <i>Convenience method to create a job execution</i><br>
      * <br>
-     * This creates a {@link Execution} object that gets added to the job engine at a specified time.
+     * This creates a {@link Execution} object that gets added to the job engine at
+     * a specified time.
      * 
      * @param parameters needed parameters to do the job
-     * @param maturity specified time for the execution
+     * @param maturity   specified time for the execution
      * @return job execution ID
      */
     public Long createPlannedExecution(T parameters, LocalDateTime maturity) {
@@ -178,8 +192,8 @@ public abstract class WorkerWith<T> extends BaseWorker {
      * This creates a batch of {@link Execution} objects
      * 
      * @param parametersList list of needed parameters to do the batch
-     * @param priority priority queuing
-     * @param maturity specified time for the execution
+     * @param priority       priority queuing
+     * @param maturity       specified time for the execution
      * @return batch ID
      */
     public Long createBatchExecutions(List<T> parametersList, Boolean priority, LocalDateTime maturity) {
@@ -203,9 +217,11 @@ public abstract class WorkerWith<T> extends BaseWorker {
     }
 
     /**
-     * This creates a chain of {@link Execution} objects, so when the first one gets executed it will bring all its chained friends.
+     * This creates a chain of {@link Execution} objects, so when the first one gets
+     * executed it will bring all its chained friends.
      * 
-     * @param parametersList list of needed parameters to do the job in the order of the execution chain
+     * @param parametersList list of needed parameters to do the job in the order of
+     *                       the execution chain
      * @return chain ID
      */
     public Long createChainedExecutions(List<T> parametersList) {
@@ -213,11 +229,13 @@ public abstract class WorkerWith<T> extends BaseWorker {
     }
 
     /**
-     * This creates a chain of {@link Execution} objects, so when the first one gets executed it will bring all its chained friends.
+     * This creates a chain of {@link Execution} objects, so when the first one gets
+     * executed it will bring all its chained friends.
      * 
-     * @param parametersList list of needed parameters to do the job in the order of the execution chain
-     * @param priority priority queuing
-     * @param maturity specified time for the execution
+     * @param parametersList list of needed parameters to do the job in the order of
+     *                       the execution chain
+     * @param priority       priority queuing
+     * @param maturity       specified time for the execution
      * @return chain ID
      */
     public Long createChainedExecutions(List<T> parametersList, Boolean priority, LocalDateTime maturity) {
@@ -237,7 +255,8 @@ public abstract class WorkerWith<T> extends BaseWorker {
                 chainedPreviousExecutionId = execution.getId();
                 continue;
             }
-            Execution execution = createExecution(parameters, priority, maturity, null, chainId, chainedPreviousExecutionId);
+            Execution execution = createExecution(parameters, priority, maturity, null, chainId,
+                    chainedPreviousExecutionId);
             chainedPreviousExecutionId = execution.getId();
 
             workhorseController.addExecutionAtEndOfChain(jobId, chainId, execution);
