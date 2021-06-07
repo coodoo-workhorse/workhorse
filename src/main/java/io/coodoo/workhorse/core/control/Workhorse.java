18681ad9f3302c01a60c3b18927cda09a8617cd6
Yvaris Djouda <yvaris.djouda@coodoo.de>
2021-06-07 17:19:10 +0200

    format respected

--- src/main/java/io/coodoo/workhorse/core/control/Workhorse.java
+++ src/main/java/io/coodoo/workhorse/core/control/Workhorse.java
@@ -2,7 +2,6 @@ package io.coodoo.workhorse.core.control;
 
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
