bdee897bcfd22bad84d213bbfed0a403630cd30b
Yvaris Djouda <yvaris.djouda@coodoo.de>
2021-08-06 09:56:48 +0200

    format violations fixed

--- src/main/java/io/coodoo/workhorse/core/control/WorkhorseController.java
+++ src/main/java/io/coodoo/workhorse/core/control/WorkhorseController.java
@@ -3,7 +3,6 @@ package io.coodoo.workhorse.core.control;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
